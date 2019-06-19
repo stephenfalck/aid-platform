4b2834e76ad872e4615296901765bcb60cee3246
Stephen Falck <stephenvfalck@gmail.com>
2019-06-19 13:33:52 +0100

    changed no requests component to a card

--- client/src/my_requests/no_requests_component.js
+++ client/src/my_requests/no_requests_component.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import { Grid, Typography, Paper } from '@material-ui/core';
+import { Grid, Typography, Card, CardContent, 
+    CardActions, Button, CardHeader } from '@material-ui/core';
+import { Link } from "react-router-dom";
 
 const NoRequests = (props) => {
     return (
         <Grid item container alignContent={'center'} justify={'center'} xs={12}>
             <Grid item sm={8} xs={10}>
-                <Paper elevation={3} id="no-requests-paper">
-                    <Typography variant='h5'>
-                        No requests. Please go to the map page to add a new request.
-                    </Typography>
-                </Paper>
+                <Card id="no-requests-paper">
+                    <CardHeader 
+                        title="No Requests"
+                        style={{textAlign: 'center'}}
+                    />
+                    <CardContent>
+                        <Typography variant='subtitle1' className="description-body" align="center">
+                            Please go to the map page to add a new request.
+                        </Typography>
+                    </CardContent>
+                    <CardActions style={{justifyContent: 'center'}}>
+                    <Link to='/' style={{textDecoration: 'none'}}>
+                        <Button 
+                            variant="contained" 
+                            size="small" 
+                            color="secondary" 
+                            onClick={null}
+                            style={{textDecoration: 'none'}}
+                        >
+                            Add request
+                        </Button>
+                        </Link>
+                    </CardActions>
+                </Card>
             </Grid>
         </Grid>
     )
